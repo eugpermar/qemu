1877d7e80d5b91ca936ca852c3aa2ec7262fe963
Eugenio Pérez <eperezma@redhat.com>
2021-04-22 10:13:16 +0200

    virtio: Add VIRTIO_F_QUEUE_STATE
    Implementation of RFC of device state capability:
    https://lists.oasis-open.org/archives/virtio-comment/202012/msg00005.html
    
    With this capability, vdpa device can reset it's index so it can start
    consuming from shadow virtqueue (SVQ), that start with state 0. Another
    approach would be to make SVQ to start forwarding from the state of the
    device when the later is stopped, but this device capability is needed
    at the destination of live migration anyway.
    
    The use case is to test SVQ with virtio-pci vdpa (vp_vdpa) with nested
    virtualization. Spawning a L0 qemu with a virtio-net device, use
    vp_vdpa driver to handle it in the guest, and then spawn a L1 qemu using
    that vdpa device. When L1 qemu calls device to set a new state though
    vdpa ioctl, vp_vdpa should set each queue state though virtio
    VIRTIO_PCI_COMMON_Q_AVAIL_STATE.
    
    Since this is only for testing vhost-vdpa, it's added here before of
    proposing to kernel code. No effort is done for checking that device
    can actually change its state, its layout, or if the device even
    supports to change state at all. These will be added in the future.
    
    Also, a modified version of vp_vdpa that allows to set these in PCI
    config is needed.
    
    TODO: Check for feature enabled and split in virtio pci config
    
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>

--- hw/virtio/virtio-pci.c
+++ hw/virtio/virtio-pci.c
@@ -1216,6 +1216,9 @@ static uint64_t virtio_pci_common_read(void *opaque, hwaddr addr,
     case VIRTIO_PCI_COMMON_Q_USEDHI:
         val = proxy->vqs[vdev->queue_sel].used[1];
         break;
+    case VIRTIO_PCI_COMMON_Q_AVAIL_STATE:
+        val = virtio_queue_get_last_avail_idx(vdev, vdev->queue_sel);
+        break;
     default:
         val = 0;
     }
@@ -1298,6 +1301,8 @@ static void virtio_pci_common_write(void *opaque, hwaddr addr,
                        proxy->vqs[vdev->queue_sel].avail[0],
                        ((uint64_t)proxy->vqs[vdev->queue_sel].used[1]) << 32 |
                        proxy->vqs[vdev->queue_sel].used[0]);
+            virtio_queue_set_last_avail_idx(vdev, vdev->queue_sel,
+                        proxy->vqs[vdev->queue_sel].state);
             proxy->vqs[vdev->queue_sel].enabled = 1;
         } else {
             virtio_error(vdev, "wrong value for queue_enable %"PRIx64, val);
@@ -1320,6 +1325,9 @@ static void virtio_pci_common_write(void *opaque, hwaddr addr,
         break;
     case VIRTIO_PCI_COMMON_Q_USEDHI:
         proxy->vqs[vdev->queue_sel].used[1] = val;
+        break;
+    case VIRTIO_PCI_COMMON_Q_AVAIL_STATE:
+        proxy->vqs[vdev->queue_sel].state = val;
         break;
     default:
         break;
@@ -1909,6 +1917,7 @@ static void virtio_pci_reset(DeviceState *qdev)
         proxy->vqs[i].desc[0] = proxy->vqs[i].desc[1] = 0;
         proxy->vqs[i].avail[0] = proxy->vqs[i].avail[1] = 0;
         proxy->vqs[i].used[0] = proxy->vqs[i].used[1] = 0;
+        proxy->vqs[i].state = 0;
     }
 
     if (pci_is_express(dev)) {
