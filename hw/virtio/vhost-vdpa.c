2da3b21592508020e2321048c4d4a47df6aba412
Eugenio Pérez <eperezma@redhat.com>
2022-06-15 20:12:32 +0200

    [IMPROVE ME] Add vhost_vdpa_last_dev

--- hw/virtio/vhost-vdpa.c
+++ hw/virtio/vhost-vdpa.c
@@ -357,6 +357,11 @@ static bool vhost_vdpa_first_dev(struct vhost_dev *dev)
     return v->index == 0;
 }
 
+static bool vhost_vdpa_last_dev(const struct vhost_dev *dev)
+{
+    return dev->vq_index + dev->nvqs == dev->vq_index_end;
+}
+
 static int vhost_vdpa_get_dev_features(struct vhost_dev *dev,
                                        uint64_t *features)
 {
@@ -1252,7 +1257,7 @@ static int vhost_vdpa_dev_start(struct vhost_dev *dev, bool started)
         vhost_vdpa_host_notifiers_uninit(dev, dev->nvqs);
     }
 
-    if (dev->vq_index + dev->nvqs != dev->vq_index_end) {
+    if (!vhost_vdpa_last_dev(dev)) {
         return 0;
     }
 
